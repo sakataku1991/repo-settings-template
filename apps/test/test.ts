a6d8cd4e9090aa6391ed8b78dd94ac1424ba42c1
sakataku1991 <sakataku1991@gmail.com>
2024-12-31 22:20:47 +0900

    feat: 「package.json」に「TypeScript」を追加した

---
+++ apps/test/test.ts
@@ -0,0 +1,9 @@
+// 動作確認用 TypeScript ファイル
+
+const message:string = "Hello, ESLint!";
+
+function greet(name: string){
+  console.log(`${message} Nice to meet you, ${name}!`);
+}
+
+greet("TypeScript");
